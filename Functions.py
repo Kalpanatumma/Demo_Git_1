75de290a75280b4b05bd0299d831730ec5123f34
Kalpanatumma <120768473+Kalpanatumma@users.noreply.github.com>
2023-03-08 23:25:21 +0530

    Removed functionname2 from Functions.py

--- Functions.py
+++ Functions.py
@@ -2,13 +2,6 @@ def functionname1(name,age):
     print("this is a function",name,age) 
 functionname1('Kalpana',1)
 
-def functionname2(a,b):
-    return a+b 
-w=functionname2(3,1)
-x=functionname2(5,8)
-print(w)
-print(x)
-
 def functionname3(a,b):
     return a-b 
 w=functionname3(3,1)
